feb43b52c335fbb2b74efc2e3096896528847607
drizles <dougrizio@gmail.com>
2020-06-29 00:47:35 -0400

    Fix: initial project files

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,5 +3,9 @@ from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(calculator, Calculator)
+
 if __name__ == '__main__':
     unittest.main()

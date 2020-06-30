656ddee34165c31d993e5d682be3be3395bbeadc
Mike Rock <mr392@njit.edu>
2020-06-29 21:48:18 -0400

    Task: Added generator and test

--- Tests/test_RandomGenerators.py
+++ Tests/test_RandomGenerators.py
@@ -1,16 +1,16 @@
 import unittest
-import RandomGenerators.RandomGenerators
+import Calc.RandomGenerators
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         #List evaluates to False if empty
-        test_list = RandomGenerators.RandomGenerators.list_generator(2)
+        test_list = Calc.RandomGenerators.list_generator(2)
 
         self.assertTrue(test_list)
 
     def test_random_item(self):
         #test default list count
-        test_list = RandomGenerators.RandomGenerators.list_generator(2)
+        test_list = Calc.RandomGenerators.list_generator(2)
         self.assertEqual(len(test_list), 100)
 
 

95108e7b9ded40fd2f2704c28ad4c00bf2360f67
Mike Rock <mr392@njit.edu>
2020-07-01 22:09:00 -0400

    Feature: confidence interval

--- Stats/ConfidenceInterval.py
+++ Stats/ConfidenceInterval.py
@@ -8,11 +8,16 @@ population = [1, 5, 9, 5, 3, 1, 8, 8]
 def confidence_interval(sample):
    calc = Calculator()
    stats = Statistics()
-   confidence_level = .95
+
    sample_size = len(sample) - 1
    sample_mean = stats.get_mean(population)
    subtract_mean_result = []
    squared_list = []
+   total = 0
+   #taken from t_distribution chart https://www.statisticshowto.com/probability-and-statistics/confidence-interval/#CISample
+   t_distribution = 2.262
+
+   confidence_level = .95
 
    for item in sample:
        result = item - sample_mean
@@ -22,10 +27,38 @@ def confidence_interval(sample):
        squared_result = Calculator.square(calc, num)
        squared_list.append(squared_result)
 
+   for num in squared_list:
+       total += num
+
+   total2 = total / sample_size
+
+   sample_deviation = Calculator.squareroot(calc, total2)
+
+   conf = (1 - confidence_level) / 2
+
+   ci = sample_deviation / Calculator.squareroot(calc,sample_size + 1)
+
+   interval = calc.multiply(ci, t_distribution)
+
+   lower_end = sample_mean - interval
+   upper_end = sample_mean + interval
+
+
+
+
+
    print(sample_size)
    print(sample_mean)
    print(subtract_mean_result)
    print(squared_list)
+   print(total)
+   print(total2)
+   print("sample deviation:" + str(sample_deviation))
+   print (conf)
+   print(interval)
+   print("Confidence interval")
+   print(lower_end)
+   print(upper_end)
 
 
 

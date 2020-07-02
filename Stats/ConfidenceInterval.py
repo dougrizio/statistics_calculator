c6d954bdfdcc2426451601975f7169863217a8b5
Mike Rock <mr392@njit.edu>
2020-07-01 21:42:22 -0400

    Fix: get mean call

--- Stats/ConfidenceInterval.py
+++ Stats/ConfidenceInterval.py
@@ -1,15 +1,16 @@
-from Stats.Mean import mean
-from Stats.Statistics import Statistics
+
 from Calc.Calculator import Calculator
+from Stats.Statistics import Statistics
 
 population = [1, 5, 9, 5, 3, 1, 8, 8]
 
 
 def confidence_interval(sample):
-   self = Calculator()
+   calc = Calculator()
+   stats = Statistics()
    confidence_level = .95
    sample_size = len(sample) - 1
-   sample_mean = mean(sample)
+   sample_mean = stats.get_mean(population)
    subtract_mean_result = []
    squared_list = []
 
@@ -18,7 +19,7 @@ def confidence_interval(sample):
        subtract_mean_result.append(result)
 
    for num in subtract_mean_result:
-       squared_result = Calculator.square(self, num)
+       squared_result = Calculator.square(calc, num)
        squared_list.append(squared_result)
 
    print(sample_size)

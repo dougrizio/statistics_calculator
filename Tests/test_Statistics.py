0be6f0dd05f8d8e4022d7646c39ab9c198686d57
Mike Rock <mr392@njit.edu>
2020-07-07 20:28:01 -0400

    fix: random tests

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -160,12 +160,12 @@ class MyTestCase(unittest.TestCase):
 
     def test_something(self):
         # List evaluates to False if empty
-        test_list = Stats.RandomGenerators.list_generator(2)
+        test_list = Stats.RandomGenerators.list_generator()
         self.assertTrue(test_list)
 
     def test_random_item(self):
         # test default list count
-        test_list = Stats.RandomGenerators.list_generator(2)
+        test_list = Stats.RandomGenerators.list_generator()
         self.assertEqual(len(test_list), 100)
 
 

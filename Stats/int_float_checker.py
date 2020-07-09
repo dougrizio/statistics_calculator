23fd0b15dd071f725341f8aaaafee78dc855d621
Mike Rock <mr392@njit.edu>
2020-07-08 21:59:51 -0400

    fix: number checker

--- Stats/int_float_checker.py
+++ Stats/int_float_checker.py
@@ -1,7 +1,19 @@
 #in progress
-def check_for_number_parameters (a = "c", b = None, c = None, d = None):
-    answer = 0
+answer = 0
+def check_for_number_parameters (a = 0, b = 0, c = 0, d = 0):
 
-    if (int(a) or float(a)) and (int(b) or float(b)):
-        pass
 
+#    if (int(a) or float(a)) and (int(b) or float(b)):
+ #       pass
+    try:
+        if (int(a) or float(a)) + (int(b) or float(b)) + (int(c) or float(c)) + (int(d) or float(d)):
+            pass
+
+
+    except:
+        print("Values must be integers or floats!")
+
+
+
+
+check_for_number_parameters(0,1,)
